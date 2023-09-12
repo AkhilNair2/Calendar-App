422a05f46dc550f705d17961e5ed846a7ff9000a
AkhilNair2 <akhil_nair@live.co.uk>
2023-09-12 21:33:39 +0100

    updating the coding

--- starter/assets/JS/script.js
+++ starter/assets/JS/script.js
@@ -14,22 +14,58 @@ $(document).ready(function(){
         localStorage.setItem(timeID, value);
 
 
-        $(".notification").addClass('Show');
+        $(".notification").addClass('show');
         
 
         setTimeout(function(){
-            $(".notification").removeClass("Show");
+            $(".notification").removeClass("show");
         }, 5000)
     })
 
 function hourUpdate(){
     let currentHour = moment().hours();
-    console.log(currentHour);
+    
+
+    for (let i = 0; i < $(".time-block").length; i++){
+        let hour = parseInt($(".time-block")[i].getAttribute("id").split("-")[1]);
+        console.log(hour)
+        console.log(currentHour)
+        if (hour < currentHour) {
+            $(".time-block")[i].classList.add("past")
+        } else if(hour === currentHour){
+            $(".time-block")[i].classList.add("past")
+            $(".time-block")[i].classList.remove("present")
+        } else {
+            $(".time-block")[i].classList.remove("past")
+            $(".time-block")[i].classList.remove("present")
+            $(".time-block")[i].classList.add("future")
+
+
+        }
+
+    }
+
+    
 
 
 }
 
 hourUpdate();
+
+let interval = setInterval(hourUpdate, 15000);
+
+$("#hour-9.description").val(localStorage.getItem("hour-9"))
+$("#hour-10.description").val(localStorage.getItem("hour-10"))
+$("#hour-11.description").val(localStorage.getItem("hour-11"))
+$("#hour-12.description").val(localStorage.getItem("hour-12"))
+$("#hour-13.description").val(localStorage.getItem("hour-13"))
+$("#hour-14.description").val(localStorage.getItem("hour-14"))
+$("#hour-15.description").val(localStorage.getItem("hour-15"))
+$("#hour-16.description").val(localStorage.getItem("hour-16"))
+$("#hour-17.description").val(localStorage.getItem("hour-17"))
+
+
+$("#currentDay").text(moment().format("dddd, MMMM, Do"))
 
 
 
